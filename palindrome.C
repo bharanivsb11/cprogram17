ae19bca678918fce17712f6731dbcaf925c4a435
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-22 16:24:22 +0530

    palindrome.C

---
+++ palindrome.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,num,digit,reverse=0;
+printf("enter the number",n);
+scanf("%d",&n);
+num=n;
+while(n!=0)
+{
+digit=n%10;
+n=n/10;
+reverse=reverse*10*digit;
+}
+if(num==reverse)
+printf("yes");
+else
+printf("no");
+return 0;
+}
